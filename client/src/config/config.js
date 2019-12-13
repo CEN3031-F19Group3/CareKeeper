19dcf6e6d45d8bd95f316c37cab090ef7efa459f
Scott <schas@ufl.edu>
2019-12-12 23:51:52 -0500

    baseurl

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,10 +2,8 @@ const CLIENT_ID = process.env.CLIENT_ID || '0oa1rnx5pw6YOvfOP357';
 const ISSUER = process.env.ISSUER || 'https://dev-454334.okta.com/oauth2/default';
 
 const inDevelopment = process.env.NODE_ENV === "development";
-const baseURL =
-    inDevelopment
-        ? `http://localhost:8000`
-        : "https://carekeeper.herokuapp.com";
+const baseURL = "https://carekeeper.herokuapp.com";
+console.log(baseURL)
 
 export default {
   oidc: {
